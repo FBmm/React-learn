da45fa027f4fdeb015aa8f7b2b95bc9387d2d138
FBm_laptop <wuq_xd@163.com>
2020-06-15 01:05:13 +0800

    html-webpack-plugin

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,4 +1,16 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+console.log(global)
 module.exports = {
     mode: 'development', // production development
     entry: './index.js',
+    // output: {
+    // path: path.resolve(__dirname, './dist'),
+    // filename: 'index_bundle.js'
+    // },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'webpack demo',
+        template: 'index.html',
+        time: new Date().toLocaleString(),
+    })],
 }
